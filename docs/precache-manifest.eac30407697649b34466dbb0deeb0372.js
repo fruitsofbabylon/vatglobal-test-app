2bd5e07080dd72beb2aaa8ecfaf0ff742b75e7b4
Anna Orlova <Anny412@yandex.ru>
2019-04-03 01:51:15 +0100

    create github pages build

---
+++ docs/precache-manifest.eac30407697649b34466dbb0deeb0372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d360cf6fae1caa50f1a6",
+    "url": "/static/js/main.0c1dd75c.chunk.js"
+  },
+  {
+    "revision": "8b09d87df3c64a39e276",
+    "url": "/static/js/2.da6de1a1.chunk.js"
+  },
+  {
+    "revision": "d360cf6fae1caa50f1a6",
+    "url": "/static/css/main.b402b1f7.chunk.css"
+  },
+  {
+    "revision": "8b09d87df3c64a39e276",
+    "url": "/static/css/2.73bbfc4c.chunk.css"
+  },
+  {
+    "revision": "35582b54c853ac633201c0474d94b9eb",
+    "url": "/index.html"
+  }
+];
