268b09fc6eb080a421793ebd518887f217d99d51
Anna Orlova <Anny412@yandex.ru>
2019-04-03 01:56:25 +0100

    fix homepage link

---
+++ docs/precache-manifest.785e242d9c94a27aef47f129514a1f00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea27d454d3d314c26e0e",
+    "url": "/vatglobal-test-app/static/js/runtime~main.92b9eeae.js"
+  },
+  {
+    "revision": "d360cf6fae1caa50f1a6",
+    "url": "/vatglobal-test-app/static/js/main.0c1dd75c.chunk.js"
+  },
+  {
+    "revision": "8b09d87df3c64a39e276",
+    "url": "/vatglobal-test-app/static/js/2.da6de1a1.chunk.js"
+  },
+  {
+    "revision": "d360cf6fae1caa50f1a6",
+    "url": "/vatglobal-test-app/static/css/main.b402b1f7.chunk.css"
+  },
+  {
+    "revision": "8b09d87df3c64a39e276",
+    "url": "/vatglobal-test-app/static/css/2.73bbfc4c.chunk.css"
+  },
+  {
+    "revision": "db2993225ec779ad366bee9e16b3d18e",
+    "url": "/vatglobal-test-app/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eac30407697649b34466dbb0deeb0372.js"
+  "/vatglobal-test-app/precache-manifest.785e242d9c94a27aef47f129514a1f00.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/vatglobal-test-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
